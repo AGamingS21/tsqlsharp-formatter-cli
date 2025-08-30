f707dfe9a47f03b5cbf87ab0f91abdb8df69cdd5
agamings21 <andrewharfst@hotmail.com>
2025-08-29 22:29:37 -0400

    feat: add project

---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace tsqlsharpcli
+{
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");    
+        }
+        
+    }
+    
+}
+
+
