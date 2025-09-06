934c3f60fdffb531b5c551c003e69e6ed91ad97f
agamings21 <andrewharfst@hotmail.com>
2025-09-06 19:32:26 -0400

    feat: include stdin and stdout for formatting option

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,9 @@
 ﻿
 using System.CommandLine;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
 using TSqlSharp.Formating;
 
 namespace tsqlsharpcli
@@ -22,16 +26,20 @@ namespace tsqlsharpcli
             var outputPathOption = new Option<string>("--output", "The output path for the file sql file.");
             outputPathOption.AddAlias("-o");
 
+            var configureFormatTextCommand = new Command("text", "format the text passed to the cli.");
+            var textOption = new Option<string>("--input", "The raw sql text.");
+            textOption.AddAlias("-i");
+            configureFormatTextCommand.AddOption(textOption);
 
-            var configureSubCommand = new Command("format", "format the file passed to the cli.");
+            var configureFileSubCommand = new Command("file", "format the file passed to the cli.");
             // Handles what happens when configure is called
-            configureSubCommand.AddOption(pathOption);
-            configureSubCommand.AddOption(outputPathOption);
-            configureSubCommand.SetHandler((path, output) =>
+            configureFileSubCommand.AddOption(pathOption);
+            configureFileSubCommand.AddOption(outputPathOption);
+            configureFileSubCommand.SetHandler((path, output) =>
             {
                 if (File.Exists(path))
                 {
-                    Format(path, output);
+                    FormatFile(path, output);
                 }
                 else
                 {
@@ -41,14 +49,27 @@ namespace tsqlsharpcli
             },
             pathOption, outputPathOption);
 
-            rootCommand.Add(configureSubCommand);
+            configureFormatTextCommand.SetHandler((text) =>
+            {
+                try
+                {
+                    FormatText(text);
+                }
+                catch
+                {
+                    Console.WriteLine("Unknown Error occured.");
+                }
+            },textOption);
+
+            rootCommand.Add(configureFormatTextCommand);
+            rootCommand.Add(configureFileSubCommand);
 
             return rootCommand.Invoke(args);
         }
 
-        static void Format(string path, string outputPath)
+        static void FormatFile(string path, string outputPath)
         {
-            if (outputPath == "")
+            if (outputPath == null)
             {
                 outputPath = path;
             }
@@ -59,14 +80,52 @@ namespace tsqlsharpcli
                 output = formatter.Format(streamReader.ReadToEnd());
             }
 
-            
+
             using (StreamWriter outputFile = new StreamWriter(outputPath, false))
             {
                 outputFile.WriteLine(output);
-            }   
+            }
         }
 
-        
+        static void FormatText(string input)
+        {
+            var ouputObj = new OutputObject();
+            try
+            {
+                var formatter = new Formatter();
+                ouputObj.Output = formatter.Format(input);
+                ouputObj.Succeeded = true;
+
+            }
+            catch (Exception ex)
+            {
+                ouputObj.Output = ex.Message;
+                ouputObj.Succeeded = false;
+            }
+
+            var options = new JsonSerializerOptions
+            {
+                TypeInfoResolver = OutputObjectSourceGenerationContext.Default,
+                // Converters = { new FooConverter() },
+            };
+            
+            Console.WriteLine(JsonSerializer.Serialize(ouputObj, options));
+        }
+
+
+    }
+
+    // Since this project is running in AOT this will need to be defined to serialize it to json.
+    [JsonSerializable(typeof(OutputObject))]
+    [JsonSerializable(typeof(bool))]        
+    [JsonSerializable(typeof(string))]        
+    internal partial class OutputObjectSourceGenerationContext : JsonSerializerContext
+    {
+    }
+    public class OutputObject
+    {
+        public bool Succeeded { get; set; }
+        public string Output { get; set; }
     }
     
 }
